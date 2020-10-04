1957e8dd4d6e27266217f35b0f2a8ddcc67ac115
carldev47 <carldev47@outlook.com>
2020-10-04 20:25:39 +0200

    Simplifying the game states Can now switch between dashboard and gameview by pressing 'Escape'

--- G_Env/SledgeHammer_C/GameManager.c
+++ G_Env/SledgeHammer_C/GameManager.c
@@ -1,17 +1,16 @@
 #include "GameManager.h"
 
-
 GameManager* Game_Startup(SDL_Renderer* renderer)
 {
-	GameManager* game = malloc(sizeof(GameManager));
-	if (game)
+	GameManager* gameManager = malloc(sizeof(GameManager));
+	if (gameManager)
 	{
-		game->renderer = renderer;
-		game->gameState = GAMESTATE_DASHBOARD;
-		game->dashboard = Dashboard_Create(renderer);
+		gameManager->renderer = renderer;
+		gameManager->gameState = GAMESTATE_DASHBOARD;
+		gameManager->dashboard = Dashboard_Create(renderer);
 	}
 
-	return game;
+	return gameManager;
 }
 
 void Game_LoadContent(GameManager* gameManager)
@@ -20,24 +19,22 @@ void Game_LoadContent(GameManager* gameManager)
 
 void Game_Update(GameManager* gameManager, int* engineRunning)
 {
-	// Retrieve cursor location and state
+	// Retrieve cursor location and the state of the keyboard and mouse;
 	SDL_Point cursorLocation = { 0, 0 };
 	Uint32 mouseState = SDL_GetMouseState(&cursorLocation.x, &cursorLocation.y);
+	Uint8* keyboardState = SDL_GetKeyboardState(0);
+	if (keyboardState[SDL_SCANCODE_ESCAPE])
+	{
+		gameManager->gameState = GAMESTATE_DASHBOARD;
+	}
 
 	// Update the game based on its state
 	if (gameManager->gameState == GAMESTATE_DASHBOARD)
 	{		
 		Dashboard_Update(gameManager->dashboard, &gameManager->gameState, cursorLocation, mouseState);
 	}
-	else if (gameManager->gameState == GAMESTATE_LOADING)
-	{
-	}
 	else if (gameManager->gameState == GAMESTATE_PLAYING)
-	{
-		
-	}
-	else if (gameManager->gameState == GAMESTATE_PAUSED)
-	{
+	{	
 	}
 	else if (gameManager->gameState == GAMESTATE_GAMEOVER)
 	{
@@ -52,24 +49,13 @@ void Game_Draw(GameManager* gameManager)
 {
 	if (gameManager->gameState == GAMESTATE_DASHBOARD)
 	{
-		// Draw menu
 		Dashboard_Draw(gameManager->dashboard, gameManager->renderer);
-	}
-	else if (gameManager->gameState == GAMESTATE_LOADING)
-	{
-		// Load game
 	}
 	else if (gameManager->gameState == GAMESTATE_PLAYING)
 	{
-		// Draw game
-	}
-	else if (gameManager->gameState == GAMESTATE_PAUSED)
-	{
-		// hold drawing // show overlay
 	}
 	else if (gameManager->gameState == GAMESTATE_GAMEOVER)
 	{
-		// Draw result screen
 	}
 }
 
